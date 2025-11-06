9e0ee2f3f5669984a071e49bf8f27b37b471f23e
Yolo <79131184+yolokazi-cloud@users.noreply.github.com>
2025-11-06 11:37:45 +0200

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+          colors: {
+        customblue: '#1F4659',
+      },
+      fontFamily: {
+        caveat: ['Caveat', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+
+
+}
